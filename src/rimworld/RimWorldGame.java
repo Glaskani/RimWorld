4dcbf006e9f14ecfc061cd2bedc44a0ce74f3914
Glaskani <victor.hachard@hotmail.fr>
2018-10-05 13:57:39 +0200

    tweak

--- src/rimworld/RimWorldGame.java
+++ src/rimworld/RimWorldGame.java
@@ -13,7 +13,7 @@ public class RimWorldGame extends GameApp{
 
 	@Override
 	public void initSetting(GameSetting setting) {
-		setting.setWidth(1720);
+		setting.setWidth(720);
 		setting.setHeight(480);
 		setting.setTitle("Hello World!");
 	}
@@ -21,18 +21,27 @@ public class RimWorldGame extends GameApp{
 	public void initLevel() {
 		Level l = new Level();
 		
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
+		/*for (int i = 0; i < 0; i++) {
+			for (int j = 0; j < 0; j++) {
 				GameObject gameObject = BuilderGameObject.createGameObject()
 						.at(new Point2D(i, j))
 						.with(new Dimension2D(32, 32))
 						.with(new Texture());
 				l.addGameObject(gameObject);
 			}
-		}
+		}*/
+				
+		GameObject gameObject = BuilderGameObject.createGameObject()
+				.at(new Point2D(1, 1))
+				.with(new Dimension2D(32, 32))
+				.with(new Texture());
 		
-
-
+		GameObject gameObject2 = BuilderGameObject.createGameObject()
+				.at(new Point2D(2, 2))
+				.with(new Dimension2D(64, 32))
+				.with(new Texture())
+				.is("Test");
+		l.addGameObject(gameObject, gameObject2);
 		
 		getGameWorld().setLevel(l);
 		
