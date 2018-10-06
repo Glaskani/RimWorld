704e6a12d9da03930af7fa4f9ff5309609af8a58
Glaskani <victor.hachard@hotmail.fr>
2018-10-07 00:06:54 +0200

    Some test
    Load map from file

--- src/rimworld/RimWorldGame.java
+++ src/rimworld/RimWorldGame.java
@@ -4,11 +4,14 @@ import gameengine.app.GameApp;
 import gameengine.app.GameSetting;
 import gameengine.entities.GameObject;
 import gameengine.entities.builder.BuilderGameObject;
+import gameengine.entities.builder.BuilderGameObjectFactory;
+import gameengine.entities.builder.WrapperGameObject;
 import gameengine.entities.texture.Texture;
 import gameengine.input.UserEvent;
 import gameengine.physic.Point2D;
 import gameengine.render.Camera;
 import gameengine.world.Level;
+import gameengine.world.builder.BuilderLevel;
 import javafx.geometry.Dimension2D;
 import javafx.scene.input.KeyCode;
 
@@ -24,7 +27,7 @@ public class RimWorldGame extends GameApp {
 	}
 	@Override
 	public void initLevel() {
-		Level l = new Level();
+		Level l = BuilderLevel.buildLevel("test", "lvl1.txt",1);
 		
 		/*for (int i = 0; i < 0; i++) {
 			for (int j = 0; j < 0; j++) {
@@ -35,26 +38,54 @@ public class RimWorldGame extends GameApp {
 				l.addGameObject(gameObject);
 			}
 		}*/
-				
-		GameObject gameObject = BuilderGameObject.createGameObject()
-				.at(new Point2D(1.0, 1.0))
-				.with(new Dimension2D(32, 32))
-				.with(new Texture("57"))
-				.with(new Point2D(0.01,0.0));
-		
-		GameObject gameObject2 = BuilderGameObject.createGameObject()
-				.at(new Point2D(2.0, 2.0))
-				.with(new Dimension2D(32, 32))
-				.with(new Texture("57"))
-				.is("Test");
-		gc = gameObject;
-		l.addGameObject(gameObject, gameObject2);
+
+//		GameObject gameObject = BuilderGameObject.createGameObject()
+//				.at(new Point2D(1.0, 1.0))
+//				.with(new Dimension2D(32, 32))
+//				.with(new Texture("57"))
+//				.with(new Point2D(0.0,0.0));
+//		
+//		GameObject gameObject2 = BuilderGameObject.createGameObject()
+//				.at(new Point2D(2.0, 2.0))
+//				.with(new Dimension2D(32, 32))
+//				.with(new Texture("57"))
+//				.is("Test");
+//		gc = gameObject;
+//		System.out.println(l.getLstGameObject());
+//		l.addGameObject(gameObject, gameObject2);
 		
 		getGameWorld().setLevel(l);
 		
 	}
+	
+	@Override
+	public void initGameObject() {
+		BuilderLevel.addWrapperGameObject(new WrapperGameObject("0",new BuilderGameObjectFactory() {
+			@Override
+			public GameObject createGameObject() {
+				
+				return BuilderGameObject.createGameObject()
+						.with(new Dimension2D(32, 32))
+						.with(new Texture("57"))
+						.is("Test");
+			}
+		}));
+		
+		BuilderLevel.addWrapperGameObject(new WrapperGameObject("1",new BuilderGameObjectFactory() {
+			@Override
+			public GameObject createGameObject() {
+				
+				return BuilderGameObject.createGameObject()
+						.with(new Dimension2D(32, 32))
+						.with(new Texture("cactus"))
+						.is("Test");
+			}
+		}));
+	}
+	
 	@Override
 	public void initCamera(Camera camera) {
+		gc = getGameWorld().getLevel().getLstGameObject().get(0);
 		camera.setPosition(gc.getPosition());
 		camera.setGameObjectBinded(gc);
 	}
@@ -71,23 +102,76 @@ public class RimWorldGame extends GameApp {
 	}
 	@Override
 	public void initInput() {
-		getInput().addEvent(new UserEvent() {
+		getInput().addEvent(new UserEvent("gauche") {
 			
 			@Override
 			public void onUpdate() {
-				System.out.println("on presse Z");
+				gc.with(new Point2D(-0.05,0.0));
 			}
 			
 			@Override
 			public void onEnd() {
-				System.out.println("on release Z");
+				gc.with(new Point2D(0.0,0.0));
 			}
 			
 			@Override
 			public void onBegin() {
-				System.out.println("on commence a appuyerz Z");
+				
 			}
-		}, KeyCode.A);
+		}, KeyCode.Q);
+		
+		getInput().addEvent(new UserEvent("reculer") {
+			
+			@Override
+			public void onUpdate() {
+				gc.with(new Point2D(0.0,0.05));
+			}
+			
+			@Override
+			public void onEnd() {
+				gc.with(new Point2D(0.0,0.0));
+			}
+			
+			@Override
+			public void onBegin() {
+				
+			}
+		}, KeyCode.S);
+		getInput().addEvent(new UserEvent("droite") {
+			
+			@Override
+			public void onUpdate() {
+				gc.with(new Point2D(0.05,0.0));
+			}
+			
+			@Override
+			public void onEnd() {
+				gc.with(new Point2D(0.0,0.0));
+			}
+			
+			@Override
+			public void onBegin() {
+				
+			}
+		}, KeyCode.D);
+		getInput().addEvent(new UserEvent("devant") {
+			
+			@Override
+			public void onUpdate() {
+				gc.with(new Point2D(0.0,-0.05));
+			}
+			
+			@Override
+			public void onEnd() {
+				
+			}
+			
+			@Override
+			public void onBegin() {
+				
+			}
+		}, KeyCode.Z);
+		
 	}
 
 }
