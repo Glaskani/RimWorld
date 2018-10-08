4efa623a95fbb480505942238fe56c69554feb37
Glaskani <victor.hachard@hotmail.fr>
2018-10-08 10:40:22 +0200

    Some test
    Emitter test

--- src/rimworld/RimWorldGame.java
+++ src/rimworld/RimWorldGame.java
@@ -1,5 +1,10 @@
 package rimworld;
 
+import java.io.IOException;
+import java.util.logging.FileHandler;
+//import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 import gameengine.app.GameApp;
 import gameengine.app.GameSetting;
 import gameengine.entities.GameObject;
@@ -17,13 +22,14 @@ import gameengine.physic.collision.aabb.AABB;
 import gameengine.render.Camera;
 import gameengine.world.Level;
 import gameengine.world.builder.BuilderLevel;
-import javafx.geometry.Dimension2D;
 import javafx.geometry.Point3D;
 import javafx.scene.input.KeyCode;
 
 
 public class RimWorldGame extends GameApp {
 	GameObject gc;
+	Emitter feu;
+	private static final Logger LOGGER = Logger.getGlobal();
 	
 	@Override
 	public void initSetting(GameSetting setting) {
@@ -48,14 +54,13 @@ public class RimWorldGame extends GameApp {
 						.with(new Dimension3D(32.0, 32.0, 0.0))
 						.with(new Texture("57"))
 						.is("Test")
-						.with(1.0);
+						.with(3.0);
 			}
 		}));
 		Dimension3D d1 = new Dimension3D(32.0, 32.0,32.0);
 		BuilderLevel.addWrapperGameObject(new WrapperGameObject("1",new BuilderGameObjectFactory() {
 			@Override
 			public GameObject createGameObject() {
-				
 				return BuilderGameObject.createGameObject()
 						.with(d1)
 						.with(new Texture("cactus"))
@@ -83,10 +88,8 @@ public class RimWorldGame extends GameApp {
 	}
 	@Override
 	public void initParticle() {
-		Emitter feu = new FireEmitter();
-		feu.at(new Point2D(1.0,1.0)).with(8.0).with(new Point2D(0.0,0.0));
-		System.out.println(getManager());
-		getManager().getParticuleEngine().addEmitter(feu);
+		feu = new FireEmitter();
+		feu.at(gc.getPosition()).with(8.0).with(new Point2D(0.0,0.0));		
 		
 	}
 	@Override
@@ -99,6 +102,18 @@ public class RimWorldGame extends GameApp {
 	}
 	public static void main(String[] args) {
 		launch(args);
+		/*try {
+            FileHandler hdl = new FileHandler("crash.log");
+            hdl.setFormatter(new SimpleFormatter());
+            LOGGER.addHandler(hdl);
+            
+            Thread.setDefaultUncaughtExceptionHandler((Thread t, Throwable e) -> {
+                LOGGER.log(Level.SEVERE,"Exeption Uncaught : ",e);
+            });       
+            launch(args);
+        } catch (IOException | SecurityException ex) {
+            Logger.getLogger(RimWorldGame.class.getName()).log(Level.SEVERE, null, ex);
+        }*/		
 	}
 	@Override
 	public void initInput() {
@@ -114,7 +129,7 @@ public class RimWorldGame extends GameApp {
 			public void onBegin() {
 				getManager().getCamera().setZoom(getManager().getCamera().getZoom()-0.1);
 			}
-		}, KeyCode.SUBTRACT);
+		}, KeyCode.PAGE_DOWN);
 		getInput().addEvent(new UserEvent("ZoomIn") {
 			@Override
 			public void onUpdate() {
@@ -127,7 +142,7 @@ public class RimWorldGame extends GameApp {
 			public void onBegin() {
 				getManager().getCamera().setZoom(getManager().getCamera().getZoom()+0.1);
 			}
-		}, KeyCode.ADD);
+		}, KeyCode.PAGE_UP);
 		getInput().addEvent(new UserEvent("Left") {
 			@Override
 			public void onUpdate() {
@@ -186,15 +201,17 @@ public class RimWorldGame extends GameApp {
 		getGameEngine().getCm().addHandleCollision(new HandleCollision("test","personnage") {
 			@Override
 			public void onCollisionEnd(GameObject g1, GameObject g2) {
+				getManager().getParticuleEngine().removeEmitter(feu);
 				System.out.println("EN");
 			}
 			@Override
 			public void onCollisionBegin(GameObject g1, GameObject g2) {
 				System.out.println("IN");
+				getManager().getParticuleEngine().addEmitter(feu);
 			}
 			@Override
 			public void onCollision(GameObject g1, GameObject g2) {
-				System.out.println("REST");
+				
 			}
 		});
 	}
