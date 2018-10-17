72a8edfb7f34a874bb8f06673ad63d167efac8f8
Glaskani <victor.hachard@hotmail.fr>
2018-10-18 00:30:54 +0200

    Update PathFinding

--- src/rimworld/RimWorldGame.java
+++ src/rimworld/RimWorldGame.java
@@ -163,7 +163,7 @@ public class RimWorldGame extends GameApp {
 	}
 	@Override
 	public void initTest() {
-		new PathFinding(gc, new Point2D(27.0,7.0),getGameWorld());
+		new PathFinding(gc, new Point2D(0.0,8.0),getGameWorld());
 		
 		getGameEngine().stop();
 	}
